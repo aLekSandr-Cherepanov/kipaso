0cab0f851b027e93be7c52f20adde237071e7506
Aleksandr Cherepanov <cherepanov3496@gmail.com>
2022-05-30 13:56:30 +0300

    валидация

--- js/form_subscribe.js
+++ js/form_subscribe.js
@@ -3,6 +3,38 @@ $(document).ready(function() {
 	//E-mail Ajax Send
 	$("form").submit(function() { //Change
 		var th = $(this);
+		let search = document.forms["formSubscribe"]["formControl"].value;
+		
+		if(search === "/^([A-Za-z0-9_\-\.])+\@([A-Za-z0-9_\-\.])+\.([A-Za-z]{2,4})$/")
+		$.ajax({
+			type: "POST",
+			url: "mail.php", //Change
+			data: th.serialize()
+		}).done(function() {
+			alert("Спасибо за подписку!");
+			setTimeout(function() {
+				// Done Functions
+				th.trigger("reset");
+			}, 1000);
+		});
+		else {
+			alert("Проверьте email");
+		}
+		return false;
+	});
+
+});
+
+
+
+
+
+
+/*$(document).ready(function() {
+
+	//E-mail Ajax Send
+	$("form").submit(function() { //Change
+		var th = $(this);
 		$.ajax({
 			type: "POST",
 			url: "mail.php", //Change
@@ -17,8 +49,7 @@ $(document).ready(function() {
 		return false;
 	});
 
-});
-
+});*/
 /*function validate(form_subscribe,form_control) {
 	var reg = /^([A-Za-z0-9_\-\.])+\@([A-Za-z0-9_\-\.])+\.([A-Za-z]{2,4})$/;
 	var address = document.forms[form_subscribe].elements[form_control].value;
@@ -56,7 +87,7 @@ function formValidation() {
 
 
 function ValidateEmail(uemail) {
-	var mailformat = /^w+([.-]?w+)*@w+([.-]?w+)*(.w{2,3})+$/;
+	var mailformat = '/^w+([.-]?w+)*@w+([.-]?w+)*(.w{2,3})+$/';
 	if(uemail.value.match(mailformat)) {
 		return true;
 	}
