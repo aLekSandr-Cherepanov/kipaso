f8edad521fde9c47b9599189abc94830633e18bc
Alexander Cherepanov <super.sany96@yandex.ru>
2022-09-20 16:56:51 +0300

    добавил наброски кода для валидации формы на главной страницы кипасо

--- дописки и добавление функционала/папка для измененных файлов основного сайта/uniMail-master форма сбора почт для подписки на kipaso php js/script/form_subscribe.js
+++ дописки и добавление функционала/папка для измененных файлов основного сайта/uniMail-master форма сбора почт для подписки на kipaso php js/script/form_subscribe.js
@@ -18,3 +18,37 @@ $(document).ready(function() {
 	});
 
 });
+
+
+
+/*let search = document.querySelector('.form-control');
+let reg = "/^([A-Za-z0-9_\-\.])+\@([A-Za-z0-9_\-\.])+\.([A-Za-z]{2,4})$/";
+
+document.querySelector('.btn').onclick = function() {
+	if(search === reg){
+	(document).ready(function() {
+		
+		//E-mail Ajax Send
+		$("form").submit(function() { //Change
+			e.preventDefault();
+			var th = $(this);
+			$.ajax({
+				type: "POST",
+				url: "mail.php", //ange
+				data: th.serialize()
+			}).done(function() {
+				alert("Спасибо за подписку!");
+				setTimeout(function() {
+					// Done Functions
+					th.trigger("reset");
+				}, 1000);
+			});
+		});
+	})}	else {
+		alert("Проверьте email");
+		return false;
+	}
+
+};*/
+
+
