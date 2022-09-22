d46109e9659c021743e2d8531ad41a053a118d63
Alexander Cherepanov <super.sany96@yandex.ru>
2022-09-22 14:52:43 +0300

    начал написание js для работы с json

---
+++ дописки и добавление функционала/папка для измененныз файлов тестового сайта/написание страницы с товарами ОВЕН API JSON/script.js
@@ -0,0 +1,11 @@
+fetch("https://owen.ru/export/catalog.json?host=test.kipaso.ru&key=Tl3RqJTP1X9UZXjNYELQQ3dgNfqjDksl")
+.then(function(response){
+    return response.json();
+})
+.then(function(products){
+    let placeholder = document.querySelector(".wrapper-container");
+    let out = "";
+    for(let product of product){
+        
+    }
+})
