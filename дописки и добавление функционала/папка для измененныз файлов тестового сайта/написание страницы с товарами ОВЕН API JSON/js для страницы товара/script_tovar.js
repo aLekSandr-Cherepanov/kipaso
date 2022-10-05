4930c0a333f37d8abc67c97b1099ca0312823416
Alexander Cherepanov <super.sany96@yandex.ru>
2022-10-05 16:53:29 +0300

    начал написание кода для каточки товара

--- дописки и добавление функционала/папка для измененныз файлов тестового сайта/написание страницы с товарами ОВЕН API JSON/js для страницы товара/script_tovar.js
+++ дописки и добавление функционала/папка для измененныз файлов тестового сайта/написание страницы с товарами ОВЕН API JSON/js для страницы товара/script_tovar.js
@@ -8,16 +8,7 @@ async function getResponse(){
 
     data.innerHTML += `
         <div>
-            <p>${content[0].items[0].products[0].desc}</p>
-            <div>${content[0].items[0].products[0].docs}</div>
-            <div>${content[0].items[0].products[0].image}</div>
-            <div>${content[0].items[0].products[0].images}</div>
-            <div>${content[0].items[0].products[0].prices}</div>
-            <div>${content[0].items[0].products[0].sku}</div>
-            <div>${content[0].items[0].products[0].specs}</div>
-            <div>${content[0].items[0].products[0].thumb}</div>
-
-            
+            <p>${content[0].items[0].products[0].desc}</p>         
         </div>
     `
 
