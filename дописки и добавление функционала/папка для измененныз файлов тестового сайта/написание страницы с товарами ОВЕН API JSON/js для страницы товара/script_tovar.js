efc8e852cffc4294f616368885d365d531aba839
Alexander Cherepanov <super.sany96@yandex.ru>
2022-10-07 10:55:24 +0300

    дописал блок с фотографиями,написал блок с ценой рядом с фото,но не стилизовал

--- дописки и добавление функционала/папка для измененныз файлов тестового сайта/написание страницы с товарами ОВЕН API JSON/js для страницы товара/script_tovar.js
+++ дописки и добавление функционала/папка для измененныз файлов тестового сайта/написание страницы с товарами ОВЕН API JSON/js для страницы товара/script_tovar.js
@@ -4,16 +4,35 @@ async function getResponse(){
     content = content.categories;
 
     let data = document.querySelector('.wrapper-tovar');
+    let product = document.querySelector('.hero-photo');
+    let secondary = document.querySelector('.secondary-photo');
+    let price = document.querySelector('.container-price');
 
 
     data.innerHTML += `
         <div>
-            <p>${content[0].items[0].products[0].desc}</p>         
+            <p>${content[0].items[0].products[0].desc}</p>
+        </div>
+    `
+    product.innerHTML += `
+        <div>
+            <img class="style-photo" src="${content[0].items[0].products[0].image}">
         </div>
     `
 
+    let keys = Object.keys(content[0].items[0].products[0].images);
+    for(let key in keys) {
 
+        secondary.innerHTML += `
+        <div>
+            <img class="style-photo" src="${content[0].items[0].products[0].images[key].src}">
+        </div>
+    `
+    }
 
+    price.innerHTML += `
+            <span> Цена:${content[0].items[0].products[0].prices[1].price}</span>
+    `
 
 
 
