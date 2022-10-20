02fdf08d034633f659b0942031d2121017e0d41c
Alexander Cherepanov <super.sany96@yandex.ru>
2022-10-20 11:58:05 +0300

    начал редактирование стилей

--- дописки и добавление функционала/папка для измененныз файлов тестового сайта/написание страницы с товарами ОВЕН API JSON/js для страницы товара/script_tovar.js
+++ дописки и добавление функционала/папка для измененныз файлов тестового сайта/написание страницы с товарами ОВЕН API JSON/js для страницы товара/script_tovar.js
@@ -134,9 +134,9 @@ async function getResponse(){
 
     console.log(content);
 
-    var table = document.querySelectorAll(".table-base");
+    /*var table = document.querySelectorAll(".table-base");
     table[1].setAttribute("border", "1");
     table[2].setAttribute("border", "1");
-    table[3].setAttribute("border", "1");
+    table[3].setAttribute("border", "1");*/
 }
 getResponse();
