33dbd743b4b0fde351b642fcb6265bb4773623fc
Alexander Cherepanov <super.sany96@yandex.ru>
2022-10-24 16:07:06 +0300

    поправил логотип овен на странице товара

--- дописки и добавление функционала/папка для измененныз файлов тестового сайта/написание страницы с товарами ОВЕН API JSON/js для страницы товара/script_tovar.js
+++ дописки и добавление функционала/папка для измененныз файлов тестового сайта/написание страницы с товарами ОВЕН API JSON/js для страницы товара/script_tovar.js
@@ -20,23 +20,23 @@ async function getResponse(){
     //название товара
     title.innerHTML += `
 
-        ${content[0].items[0].products[0].name} 
+        ${content[0].items[0].products[7].name} 
 
     `
     //вывод главного,большого,фото товара
     product.innerHTML += `
-        <a class="link-photo" href="${content[0].items[0].products[0].image}">
-            <img class="hero-photo" src="${content[0].items[0].products[0].image}" alt="2ТРМ0">
+        <a class="link-photo" href="${content[0].items[0].products[7].image}">
+            <img class="hero-photo" src="${content[0].items[0].products[7].image}" alt="2ТРМ0">
         </a>
     `
 
     //вывод маленьких фото
-    let keys = Object.keys(content[0].items[0].products[0].images);
+    let keys = Object.keys(content[0].items[0].products[7].images);
     for(let key in keys) {
         secondary.innerHTML += `
         <div class="wrapper-link-photo">
-            <a class="link-photo" data-lightbox="image-tovar-group" href="${content[0].items[0].products[0].images[key].src}">
-                <img class="photos__img lazy-img" src="${content[0].items[0].products[0].images[key].src}" alt="${content[0].items[0].products[0].images[key].alt}">
+            <a class="link-photo" data-lightbox="image-tovar-group" href="${content[0].items[0].products[7].images[key].src}">
+                <img class="photos__img lazy-img" src="${content[0].items[0].products[7].images[key].src}" alt="${content[0].items[0].products[7].images[key].alt}">
             </a>
         </div>
     `
@@ -44,24 +44,24 @@ async function getResponse(){
 
     //вывод цены
     price.innerHTML += `
-        ${content[0].items[0].products[0].prices[0].price}
+        ${content[0].items[0].products[7].prices[0].price}
     `
 
     //блок с инфой из desc(json)
     desc.innerHTML += `
-        ${content[0].items[0].products[0].desc}
+        ${content[0].items[0].products[7].desc}
     `
     //блок с инфой из specs(json)
     specs.innerHTML += `
-        ${content[0].items[0].products[0].specs}
+        ${content[0].items[0].products[7].specs}
     `
     //блок с инфой из docs-заголовок документация
     headerDocumentation.innerHTML += `
-        ${content[0].items[0].products[0].docs[0].name}
+        ${content[0].items[0].products[7].docs[0].name}
     `
 
     //содержание блока документация
-    let doc = Object.keys(content[0].items[0].products[0].docs[0].items);
+    let doc = Object.keys(content[0].items[0].products[7].docs[0].items);
     for(let key in doc) {
 
         contentDocumentation.innerHTML += `
@@ -70,8 +70,8 @@ async function getResponse(){
                 <img class="doc-icon" src="/assets/templates/new/img/doc-oven.png" alt="">
             </div>
             <div class="block-name-certificates">
-                <a class="link-doc" href="${content[0].items[0].products[0].docs[1].items[key].link}">
-                    ${content[0].items[0].products[0].docs[1].items[key].name}
+                <a class="link-doc" href="${content[0].items[0].products[7].docs[0].items[key].link}">
+                    ${content[0].items[0].products[7].docs[0].items[key].name}
                 </a><br>
             </div>
         </div>
@@ -80,11 +80,11 @@ async function getResponse(){
 
     //блок с инфой из docs-заголовок сертификаты
     headerCertificates.innerHTML += `
-        ${content[0].items[0].products[0].docs[1].name}
+        ${content[0].items[0].products[7].docs[1].name}
     `
 
     //содержание блока сертификаты
-    let certificates = Object.keys(content[0].items[0].products[0].docs[1].items);
+    let certificates = Object.keys(content[0].items[0].products[7].docs[1].items);
     for(let key in certificates) {
 
         contentCertificates.innerHTML += `
@@ -93,8 +93,8 @@ async function getResponse(){
                 <img class="doc-icon" src="/assets/templates/new/img/txt-oven.png" alt="">
             </div>
             <div class="block-name-certificates">
-                <a class="link-doc" href="${content[0].items[0].products[0].docs[1].items[key].link}">
-                    ${content[0].items[0].products[0].docs[1].items[key].name}
+                <a class="link-doc" href="${content[0].items[0].products[7].docs[1].items[key].link}">
+                    ${content[0].items[0].products[7].docs[1].items[key].name}
                 </a><br>
             </div>
         </div>
@@ -102,11 +102,11 @@ async function getResponse(){
     }
         //блок с инфой из docs-заголовок ПО
         headerPo.innerHTML += `
-                ${content[0].items[0].products[0].docs[2].name}
+                ${content[0].items[0].products[7].docs[2].name}
         `
 
         //содержание блока ПО
-        let keysPo = Object.keys(content[0].items[0].products[0].docs[2].items);
+        let keysPo = Object.keys(content[0].items[0].products[7].docs[2].items);
         for(let key in keysPo) {
 
             contentPo.innerHTML += `
@@ -115,8 +115,8 @@ async function getResponse(){
                     <img class="doc-icon" src="/assets/templates/new/img/app-oven.png" alt="">
                 </div>
                 <div class="block-name-po">
-                  <a class="link-doc" href="${content[0].items[0].products[0].docs[2].items[key].link}">
-                    ${content[0].items[0].products[0].docs[2].items[key].name}
+                  <a class="link-doc" href="${content[0].items[0].products[7].docs[2].items[key].link}">
+                    ${content[0].items[0].products[7].docs[2].items[key].name}
                   </a><br>
                 </div>
             </div>
@@ -124,19 +124,22 @@ async function getResponse(){
         }
 
     //содержание блока prices 
-    let keysPrice = Object.keys(content[0].items[0].products[0].prices);
+    let keysPrice = Object.keys(content[0].items[0].products[7].prices);
     for(let key in keysPrice) {
 
         wrapperPrice.innerHTML += `
         <div class="wrapper-price_block">
-            <div class="name-prodict">${content[0].items[0].products[0].prices[key].name}</div>
-            <div class="price-prodict">${content[0].items[0].products[0].prices[key].price} ₽</div>
+            <div class="name-prodict">${content[0].items[0].products[7].prices[key].name}</div>
+            <div class="price-prodict">${content[0].items[0].products[7].prices[key].price} ₽</div>
         </div>
     `
     }
 
     //добавил класс блоку,для того чтобы его скрыть из верстки
     //document.querySelector('.table-base').classList.add('block-none');
+
+    // добавляет класс к блоку ПО чтобы его скрыть
+    //document.querySelector(".po").style.display = "none";   
 
 
     console.log(content);
