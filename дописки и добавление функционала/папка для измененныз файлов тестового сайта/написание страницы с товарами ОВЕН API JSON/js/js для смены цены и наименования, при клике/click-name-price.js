02e0eda1fd6968088b24490e6b95b018e5344a47
Alexander Cherepanov <super.sany96@yandex.ru>
2023-01-30 10:02:19 +0300

    мелкие правки

--- дописки и добавление функционала/папка для измененныз файлов тестового сайта/написание страницы с товарами ОВЕН API JSON/js/js для смены цены и наименования, при клике/click-name-price.js
+++ дописки и добавление функционала/папка для измененныз файлов тестового сайта/написание страницы с товарами ОВЕН API JSON/js/js для смены цены и наименования, при клике/click-name-price.js
@@ -25,7 +25,7 @@ window.addEventListener('click', function(event){
 })
 
 //собираем данные внутри кнопки(наименование,цена)
-function testFunction() {
+function buttonFunction() {
     let productName = document.querySelector('.string-price').innerText;
     let productPrice = document.querySelector('.name-modification').innerText;
 
@@ -47,10 +47,26 @@ function testFunction() {
 });*/
 
 //запускаем код через 2 сек после загрузки страницы
-window.setTimeout(testFunction, 2000);
+window.setTimeout(buttonFunction, 2000);
 
 
 //код для подсветки активной кнопки
+
+/*
+Алгоритм:
+    1 Устанавливаем active классы вручную на кнопку и на блок-контента
+    2 Скрываем в CSS все блоки контента, кроме активного
+    3 в JS обрабатываем клик по кнопкам
+    4 При клике:
+        - проверяем содержание active класса, чтобы избежать лишних вызовов ф-ций
+        - очищаем циклом все active - классы у кнопок и у блок-контента
+            для этого напишем функцию очистки классов
+        - устанавливаем новый активный класс
+            для этого напишем функцию установки активного класса
+*/
+
+
+
 const button = Array.from(document.querySelectorAll('.list-modification'));
 
 const clearActiveClass = (element, className = 'blue') => {
